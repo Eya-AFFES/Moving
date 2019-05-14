4d0cb2ebe3a8924bb5808b5e9604ef6129cb3640
Eya-AFFES <37873048+Eya-AFFES@users.noreply.github.com>
2019-05-14 15:45:53 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -18,37 +18,9 @@ class SpeakmeterSkill(MycroftSkill):
         super(SpeakmeterSkill, self).__init__(name="SpeakmeterSkill")
         
     def initialize(self):
-        MV_F_intent = IntentBuilder("MVFIntent").require("moveKeyword").require("Mydistance").build()
+        MV_F_intent = IntentBuilder("MVFIntent").require("moveKeyword")..build()
         self.register_intent(MV_F_intent ,self.handle_MV_F_intent)
-    """
-    def handle_MV_F_intent(self, message):
-        self.speak_dialog("MV.F")
-        self.speak('for{}'.format(msg))
-        dist=str(message.data.get("Mydistance"))
-        msg="MF"
-        T = ("1","one","2","two","3","three","4","four","5","five","6","six","7","seven","8","eight","9","nine")
-        if (T[0] in dist) or (T[1] in dist):
-          msg+="1\n"
-        elif (T[2] in dist) or (T[3] in dist):
-          msg+="2\n"
-        elif (T[4] in dist) or (T[5] in dist):
-          msg+="3\n"
-        elif (T[6] in dist) or (T[7] in dist):
-          msg+="4\n"  
-        elif (T[8] in dist) or (T[9] in dist):
-          msg+="5\n"
-        elif (T[10] in dist) or (T[11] in dist):
-          msg+="6\n"  
-        elif (T[12] in dist) or (T[13] in dist):
-          msg+="7\n" 
-        elif (T[14] in dist) or (T[15] in dist):
-          msg+="8\n" 
-        elif (T[16] in dist) or (T[17] in dist):
-          msg+="9\n" 
-        else: 
-          msg="MVF\n"
-        ser00.write(bytes(msg, 'utf-8'))  
- """
+
     def handle_MV_F_intent(self, message):
         eya="required meters"
         dist=str(message.data.get("Mydistance"))
